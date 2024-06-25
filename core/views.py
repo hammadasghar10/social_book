e8605106c3cd40992178afc3b9464d29aa7e44ce
hammadasghar10 <hammad301gujjar@gmail.com>
2024-06-25 15:45:38 +0500

    create url of user from index to profile page

--- core/views.py
+++ core/views.py
@@ -47,8 +47,8 @@ def like_post(request):
 
 @login_required(login_url="signin")
 def profile(request,pk):
-   userobject= User.objects.get(username=pk)
-   user_profile = Profile.objects.get(user=userobject)
+  # userobject= User.objects.get(username=pk)
+   user_profile = Profile.objects.get(user__username=pk)
    user_posts=Post.objects.filter(user=pk)
    user_post_length=len(user_posts)
    return render(request,"profile.html",context={"user_profile":user_profile,
